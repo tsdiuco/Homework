f8b7a195403b6eedac1eaf813daf75ad475919fd
tsdiuco <tsdiuco@gmail.com>
2020-10-31 13:53:49 -0700

    Completed Project 6

--- project6/project6.cpp
+++ project6/project6.cpp
@@ -17,45 +17,95 @@ using namespace std;
 
 List::List()
 {
+   length = 0;
 }
 
 List::~List()
 {
+   while (!IsEmpty())
+      DeleteItemH();
 }
 
 bool List::IsEmpty() const
 {
-   return true;
+   return GetLength() == 0;
 }
-
 
 int List::GetLength() const
 {
-   return 0;
+   return length;
 }
 
 void List::PutItemH(const itemType newItem)
 {
+   node* tmp = new node;
+   if (IsEmpty())
+      tmp->next = NULL;
+   else
+      tmp->next = head;
+   tmp->item = newItem;
+   head = tmp;
+   length++;
 }
 
 itemType List::GetItemH() const
 {
+   if (!IsEmpty())
+      return head->item;
    return 1;
 }
 
 void List::DeleteItemH()
 {
+   if (!IsEmpty())
+   {
+      node* tmp = head;
+      if (length > 1)
+         head = head->next;
+      delete tmp;
+      if (length == 1)
+         head = NULL;
+      length--;
+   }
 }
 
 void List::Print() const
 {
+   node* tmp = head;
+   while (tmp != NULL)
+   {
+      cout << tmp->item << endl;
+      tmp = tmp->next;
+   }
+   delete tmp;
 }
 
 int List::Find(const itemType item) const
 {
+   int count = 0;
+   node* tmp = head;
+   while (count < length) {
+      if (tmp->item == item)
+         return count;
+      tmp = tmp->next;
+      count++;
+   }
+   delete tmp;
    return -1;
 }
 
-void DeleteItem(const itemType item)
+void List::DeleteItem(const int pos)
 {
+   if (pos == 0)
+      DeleteItemH();
+   if (pos <= length && pos > 0) {
+      node* tmp = head;
+      for (int i = 0; i < pos - 1; i++)
+         tmp = tmp->next;
+      tmp->next = tmp->next->next;
+      delete tmp;
+      if (pos == length -1)
+         tmp->next = NULL;
+   }
+   length--;
 }
