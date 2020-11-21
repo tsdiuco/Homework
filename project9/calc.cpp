bbfaacf4896ec03cdd61599b5912a1e91fdc0bf8
tsdiuco <tsdiuco@gmail.com>
2020-11-20 19:51:18 -0800

    Finished Proj 9

---
+++ project9/calc.cpp
@@ -0,0 +1,83 @@
+/*
+Class: CPSC 122-02
+Trevor Diuco
+GU Username: tdiuco
+Submitted By: Trevor Diuco
+GU Username: tdiuco
+File Name: calc.h
+To Execute: ./calc "(A+B)" 27 32
+*/
+
+#include <iostream>
+using namespace std;
+
+#include "calc.h"
+#include <cstring>
+
+Calc::Calc(int argcIn, char* argvIn[])
+{
+   inFix = new char[strlen(argvIn[1]) + 1];
+   strcpy(inFix,argvIn[1]);
+
+   if (!CheckTokens()) {
+      cout << "Failed at CheckTokens()... Exiting\n";
+      exit(EXIT_FAILURE);
+   }
+
+   values = new int[argcIn-2];
+   for (int i = 2; i < argcIn; i++)
+      values[i-2] = atoi(argvIn[i]);
+
+   stk = new Stack;
+
+   if (!CheckParens()) {
+      cout << "Failed at CheckParens()... Exiting\n";
+      exit(EXIT_FAILURE);
+   }
+}
+
+Calc::~Calc()
+{}
+
+void Calc::DisplayInFix() {
+   cout << inFix << endl;
+}
+
+bool Calc::CheckParens()
+{
+   int pos = 0;
+   while (inFix[pos] != '\0') {
+      cout << inFix[pos] << endl;
+      if (inFix[pos] == '(')
+         stk->Push(inFix[pos]);
+      if (inFix[pos] == ')'){
+         if (stk->IsEmpty())
+            return false;
+         stk->Pop();
+      }
+      pos++;
+   }
+   if (stk->IsEmpty())
+      return true;
+   return false;
+}
+
+bool Calc::CheckTokens()
+{
+   string str = "()*/+-";
+   int count = 0;
+   while (inFix[count] != '\0') {
+      if (isalpha(inFix[count])) {
+         count++;
+         continue;
+      }
+      for (int i = 0; i < str.length(); i++) {
+         if (inFix[count] == str.at(i))
+            break;
+         if (i == (str.length() - 1))
+            return false;
+      }
+      count++;
+   }
+   return true;
+}
---
+++ project9/calc.h
@@ -0,0 +1,58 @@
+/*
+Class: CPSC 122-02
+Trevor Diuco
+GU Username: tdiuco
+Submitted By: Trevor Diuco
+GU Username: tdiuco
+File Name: calc.h
+To Execute: ./calc "(A+B)" 27 32
+*/
+
+#ifndef CALC
+#define CALC
+
+#include "stack2.h"
+
+class Calc
+{
+   public:
+   /*
+   pre: none
+   post:
+      Design/Code/Test in this order:
+      1. inFix points to newly allocated memory holding infix expression
+      2. CheckTokens() has been invoked.  If it returns false, display a
+         message and exit the program.
+      3. CheckParens() has been invoked.  If it returns false, display a
+         message and exit the program.
+   */
+   Calc(int argcIn, char* argvIn[]);
+
+   ~Calc();
+
+   /*
+   pre: instance of Calc exists.
+   post: infix expression displayed
+   */
+   void DisplayInFix();
+
+   /*
+      pre:  instance of calculator exists and all input tokens are legal
+      post: Using the stack technique discussed in class, returns true
+            if parentheses are balanced, false otherwise
+   */
+   bool CheckParens();
+
+   private:
+   /*
+   pre:  invoked from constructor
+   post: returns true if all characters in the infix expression are legal,
+         false otherwise.   Legal characters are ')', '(', '+','-','*','/','A' ... 'Z'
+   */
+   bool CheckTokens();
+
+   char* inFix;     //null-terminated string that holds infix expression
+   int* values;    //pointer to an array holding expression values
+   Stack* stk;
+};
+#endif
